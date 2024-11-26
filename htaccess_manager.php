ff82c5d220e2092f2df6346f51666d65ee21c3d5
Aaron Klump <sourcecode@intheloftstudios.com>
2024-11-25 17:40:48 -0800

    Release version 0.0.1

--- htaccess_manager.php
+++ htaccess_manager.php
@@ -1,17 +1,20 @@
 #!/usr/bin/env php
 <?php
+// SPDX-License-Identifier: BSD-3-Clause
 
 $class_loader = include $_composer_autoload_path ?? __DIR__ . '/../vendor/autoload.php';
 
 use AKlump\HtaccessManager\BuildCommand;
+use AKlump\HtaccessManager\Plugin\PluginInterface;
+use AKlump\PluginFramework\GetPlugins;
 use Symfony\Component\Console\Application;
 
-$plugins = (new \AKlump\PluginFramework\GetPlugins(
+$plugins = (new GetPlugins(
   [
     __DIR__ . '/src/Plugin',
     __DIR__ . '/plugins/',
   ],
-  [\AKlump\HtaccessManager\Plugin\PluginInterface::class],
+  [PluginInterface::class],
   function ($a, $b) {
     $a = (new $a['classname']())->getPriority();
     $b = (new $b['classname']())->getPriority();
@@ -24,6 +27,6 @@ $plugins = (new \AKlump\PluginFramework\GetPlugins(
 
 $application = new Application();
 $application->setName('Htaccess Manager');
-$application->setVersion('0.0.0');
+$application->setVersion('0.0.1');
 $application->add(new BuildCommand(__FILE__, $plugins));
 $application->run();
